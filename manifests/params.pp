68c7c571a7d2274af35f1830671caa763667a505
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-02 13:15:55 -0400

    Copied the zoneminder 1.25.0 source tarball to files, and removed the tarball variable from params.pp.  I'll just push out the file via puppet.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,3 @@
 class zoneminder::params {
-  $tarball = 'http://www2.zoneminder.com/downloads/ZoneMinder-1.25.0.tar.gz'
   $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'perl', 'ffmpeg', 'build-essenail', 'libmysqlclient-dev', 'libavcodec-dev', 'libavdevice-dev', 'libavfilter-dev', 'libavformat-dev', 'libavutil-dev', 'libpostproc-dev', 'libswscale-dev', 'libjpeg-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-tar-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl']
 }
