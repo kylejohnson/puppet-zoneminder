7475f7b1a794800e36f7cf098e149a78104e79c0
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-02 21:46:43 -0400

    Removed ffmpeg and libjpeg deps.  Going to install from source

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class zoneminder::params {
-  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'perl', 'ffmpeg', 'build-essenail', 'libmysqlclient-dev', 'libavcodec-dev', 'libavdevice-dev', 'libavfilter-dev', 'libavformat-dev', 'libavutil-dev', 'libpostproc-dev', 'libswscale-dev', 'libjpeg-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-tar-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'git-core']
+  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libpostproc-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl']
   $source = 'https://github.com/mastertheknife/ZoneMinder-kfir.git'
   $configure_options = '--prefix=/opt/zm --with-libarch=lib64 --with-mysql=/usr --with-ffmpeg=/usr --with-webuser=www-data --with-webgroup=www-data --disable-debug --disable-crashtrace --enable-mmap=yes --with-webdir=/var/www/zm --with-cgidir=/usr/lib/cgi-bin ZM_SSL_LIB=openssl'
 }
