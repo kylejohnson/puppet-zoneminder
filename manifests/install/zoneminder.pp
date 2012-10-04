44e9e6a994863fc96f8ef324438c2029c432eeca
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-04 10:34:15 -0400

    Updated the zm install class to push out a config file and run it, as opposed to doing an exec with a hard coded config line, and changed some dependencies. Also moved from mastertheknife's source to nextime's source, it more recent and more better.

--- manifests/install/zoneminder.pp
+++ manifests/install/zoneminder.pp
@@ -1,28 +1,38 @@
 class zoneminder::install::zoneminder {
+
   exec { "clone-zoneminder":
-    command => "git clone $zoneminder::params::zoneminder_source zoneminder",
+    command => "svn co $zoneminder::params::zoneminder_source zoneminder",
     cwd => "/usr/local/src",
     creates => "/usr/local/src/zoneminder",
+    before => File["/usr/local/src/zoneminder/configure.sh"]
+  }
+
+  file { "/usr/local/src/zoneminder/configure.sh":
+    source => "puppet:///modules/zoneminder/configure.sh",
+    owner => 'root',
+    group => 'root',
+    mode => 0555,
+    ensure => file,
     before => Exec["configure-zm"]
   }
 
   exec { "configure-zm":
-    command => "/usr/local/src/zoneminder/configure $zoneminder::params::zoneminder_configure_options",
+    command => "/usr/local/src/zoneminder/configure.sh",
     cwd => "/usr/local/src/zoneminder",
     creates => "/usr/local/src/zoneminder/config.log",
-    before => Exec["make-zm"]
+    notify => Exec["make-zm"]
   }
 
   exec { "make-zm":
     command => "make",
     cwd => "/usr/local/src/zoneminder",
-    creates => "",
-    before => Exec["make-install-zm"]
+    refreshonly => true,
+    notify => Exec["make-install-zm"]
   }
 
   exec { "make-install-zm":
     command => "make install",
     cwd => "/usr/local/src/zoneminder",
-    creates => "",
+    refreshonly => true,
   }
 }
