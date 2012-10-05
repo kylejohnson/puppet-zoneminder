e1b22e423c09369bb80ff6cec9b25993016fe892
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-05 07:49:39 -0400

    Updated the prereqs and the ZM source

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class zoneminder::params {
-  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libpostproc-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm']
+  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8']
 
-  $zoneminder_source = 'https://github.com/mastertheknife/ZoneMinder-kfir.git'
+  $zoneminder_source = 'https://svn.unixmedia.net/public/zum/trunk/zum/'
   $zoneminder_configure_options = '--prefix=/opt/zm --with-libarch=lib64 --with-mysql=/usr --with-ffmpeg=/usr --with-webuser=www-data --with-webgroup=www-data --disable-debug --disable-crashtrace --enable-mmap=yes --with-webdir=/var/www/zm --with-cgidir=/usr/lib/cgi-bin ZM_SSL_LIB=openssl'
 
   $ffmpeg_source = 'git://source.ffmpeg.org/ffmpeg.git'
