b1b210bcdc0eeec1c4ecadfbb43dfc48651e1bc3
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-07 18:54:25 -0400

    Added db user and pass options (not currently used), and changed the install prefix

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,11 @@
 class zoneminder::params {
+  $zoneminder_db_user = 'zm'
+  $zoneminder_db_pass = 'zm'
+  
+
   $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8']
 
   $zoneminder_source = 'https://svn.unixmedia.net/public/zum/trunk/zum/'
-  $zoneminder_configure_options = '--prefix=/opt/zm --with-libarch=lib64 --with-mysql=/usr --with-ffmpeg=/usr --with-webuser=www-data --with-webgroup=www-data --disable-debug --disable-crashtrace --enable-mmap=yes --with-webdir=/var/www/zm --with-cgidir=/usr/lib/cgi-bin ZM_SSL_LIB=openssl'
 
   $ffmpeg_source = 'git://source.ffmpeg.org/ffmpeg.git'
   $ffmpeg_configure_options = '--enable-shared --enable-swscale --enable-gpl'
