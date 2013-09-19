d257704ee10e8e09c0e4dd4791b845b93f0c8a1a
Kyle Johnson <kylejohnson@users.noreply.github.com>
2013-09-18 20:33:19 -0400

    Updated ffmpeg configure options to build with h264 and webm support

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,12 +3,13 @@ class zoneminder::params {
   $zoneminder_db_pass = 'zm'
   
 
-  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8', 'apache2-mpm-prefork', 'libapache2-mod-php5', 'php5-cli']
+  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8', 'apache2-mpm-prefork', 'libapache2-mod-php5', 'php5-cli', 'libtheora-dev', 'libvorbis-dev', 'libvpx-dev', 'libx264-dev']
 
   $zoneminder_source = 'https://github.com/ZoneMinder/ZoneMinder.git'
 
   $ffmpeg_source = 'git://source.ffmpeg.org/ffmpeg.git'
-  $ffmpeg_configure_options = '--enable-shared --enable-swscale --enable-gpl'
+  $ffmpeg_configure_options = '--enable-shared --enable-swscale --enable-gpl  --enable-libx264 --enable-libvpx --enable-libvorbis --enable-libtheora'
+
 
   $libjpeg_source = 'http://downloads.sourceforge.net/project/libjpeg-turbo/1.1.1/libjpeg-turbo_1.1.1_amd64.deb?use_mirror=voxel'
 }
