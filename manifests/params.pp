d78e3faeedeb4f59e363acadeff195bec9a19f27
Kyle Johnson <kjohnson@gnulnx.net>
2014-01-08 10:23:52 -0500

    added libx264-dev to the list of prerequisites

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,8 +2,7 @@ class zoneminder::params {
   $zoneminder_db_user = 'zm'
   $zoneminder_db_pass = 'zm'
   
-
-  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8', 'apache2-mpm-prefork', 'libapache2-mod-php5', 'php5-cli', 'libtheora-dev', 'libvorbis-dev', 'libvpx-dev', 'libx264-dev']
+  $prerequisites = ['apache2', 'mysql-server', 'php5', 'php5-mysql', 'build-essential', 'libmysqlclient-dev', 'libssl-dev', 'libbz2-dev', 'libpcre3-dev', 'libdbi-perl', 'libarchive-zip-perl', 'libdate-manip-perl', 'libdevice-serialport-perl', 'libmime-perl', 'libpcre3', 'libwww-perl', 'libdbd-mysql-perl', 'libsys-mmap-perl', 'yasm', 'subversion', 'automake', 'autoconf', 'libjpeg-turbo8-dev', 'libjpeg-turbo8', 'apache2-mpm-prefork', 'libapache2-mod-php5', 'php5-cli', 'libtheora-dev', 'libvorbis-dev', 'libvpx-dev', 'libx264-dev', 'libmp4v2-dev']
 
   $zoneminder_source = 'https://github.com/ZoneMinder/ZoneMinder.git'
 
